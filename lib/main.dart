765e2fbeeb4f8ee474f002e071eee62b674a1a9e
Pedro Henrique Rocha <pedrohrocodes@gmail.com>
2021-01-21 14:43:59 -0300

    :sparkles: Adding Widgets and HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:covid19_blue_app_ui/constants.dart';
+import 'package:covid19_blue_app_ui/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,15 +7,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Covid 19 Blue UI',
       theme: ThemeData(
           scaffoldBackgroundColor: kBackgroundColor,
           fontFamily: "Poppins",
           textTheme: TextTheme(body1: TextStyle(color: kBodyTextColor))),
+      home: HomeScreen(),
     );
   }
 }
---
+++ lib/screens/info/info_screen.dart
@@ -0,0 +1,51 @@
+import 'package:covid19_blue_app_ui/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+
+class InfoScreen extends StatefulWidget {
+  @override
+  _InfoScreenState createState() => _InfoScreenState();
+}
+
+class _InfoScreenState extends State<InfoScreen> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MyHeader(
+                image: "assets/icons/coronadr.svg",
+                textTop: "Get to know",
+                textBottom: "About Covid-19",
+                offset: offset)
+          ],
+        ),
+      ),
+    );
+  }
+}
