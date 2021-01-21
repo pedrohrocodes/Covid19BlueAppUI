a379b9bc26c66418b45abc7d2b32a61c09936356
Pedro Henrique Rocha <pedrohrocodes@gmail.com>
2021-01-21 15:05:31 -0300

    :sparkles: Adding SymptomCard

--- lib/screens/info/widgets/symptom_card.dart
+++ lib/screens/info/widgets/symptom_card.dart
@@ -1,8 +1,39 @@
+import 'package:covid19_blue_app_ui/constants.dart';
 import 'package:flutter/material.dart';
 
 class SymptomCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final bool isActive;
+
+  const SymptomCard({Key key, this.image, this.title, this.isActive})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.white,
+          boxShadow: [
+            isActive
+                ? BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 20,
+                    color: kActiveShadowColor)
+                : BoxShadow(
+                    offset: Offset(0, 3), blurRadius: 6, color: kShadowColor)
+          ]),
+      child: Column(
+        children: [
+          Image.asset(image, height: 90),
+          Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
+    );
   }
 }
